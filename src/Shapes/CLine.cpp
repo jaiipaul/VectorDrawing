ca1c36d29d32aa7fab453708566aa14f579abf5d
pfriteau <78738811+pfriteau@users.noreply.github.com>
2021-04-12 21:25:10 +0200

    r

--- src/Shapes/CLine.cpp
+++ src/Shapes/CLine.cpp
@@ -39,7 +39,7 @@ CLine::CLine(string command, string type, size_t pos1){
 CLine::~CLine(){
 }
 
-void drawLine(CImage* img){
+void CLine::drawLine(CImage* img){
   opacity(img);
   int x, y, px, py;
   double pente, p;
@@ -48,36 +48,38 @@ void drawLine(CImage* img){
   int* y1;
   int* x2;
   int* y2;
-  int* tmp;
+  int  tmp;
   if (abs(_y2-_y1) > abs(_x2-_x1)){
     *x1 = _y1;
     *y1 = _x1;
     *x2 = _y2;
     *y2 = _x2;
+    cout << "1" << endl;
   } else {
     *x1 = _x1;
     *y1 = _y1;
     *x2 = _x2;
     *y2 = _y2;
+    cout << "2" << endl;
   }
-  if (*x1 > *x2) {
+  if (x1 > x2) {
     invert = 1;
-    tmp = x1;
+    tmp = *x1;
     x1 = x2;
-    x2 = tmp;
-    tmp = y1;
+    *x2 = tmp;
+    tmp = *y1;
     y1 = y2;
-    y2 = tmp;
-    tmp = y1;
+    *y2 = tmp;
+    cout << "3" << endl;
   }
   x = *x1;
   y = *y1;
-  pente = (*y2-*y1)/(*x2-*x1);
-  p = (*y2-*y1)/(*x2-*x1);
+  pente = (y2-y1)/(x2-x1);
+  p = (y2-y1)/(x2-x1);
   for (int i=0; i<(*x2-*x1+1); i++){
     x++;
     if (pente > 0.5){
-      p = (*y2-*y1)/(*x2-*x1);
+      //p = (*y2-*y1)/(*x2-*x1);
       if (pente < 0){
         y++;
       } else {
