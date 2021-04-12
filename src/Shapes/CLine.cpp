51424a653693d87351d80b1b8be1c238ef0377c7
pfriteau <78738811+pfriteau@users.noreply.github.com>
2021-04-12 20:18:09 +0200

    line

--- src/Shapes/CLine.cpp
+++ src/Shapes/CLine.cpp
@@ -41,5 +41,61 @@ CLine::~CLine(){
 }
 
 void drawLine(CImage* img){
-  //TO D0
+  opacity(img);
+  int x, y, px, py;
+  double pente, p;
+  int invert = 0;
+  int* x1;
+  int* y1;
+  int* x2;
+  int* y2;
+  int* tmp;
+  if (abs(_y2-_y1) > abs(_x2-_x1)){
+    *x1 = _y1;
+    *y1 = _x1;
+    *x2 = _y2;
+    *y2 = _x2;
+  } else {
+    *x1 = _x1;
+    *y1 = _y1;
+    *x2 = _x2;
+    *y2 = _y2;
+  }
+  if (*x1 > *x2) {
+    invert = 1;
+    tmp = x1;
+    x1 = x2;
+    x2 = tmp;
+    tmp = y1;
+    y1 = y2;
+    y2 = tmp;
+    tmp = y1;
+  }
+  x = *x1;
+  y = *y1;
+  pente = (*y2-*y1)/(*x2-*x1);
+  p = (*y2-*y1)/(*x2-*x1);
+  for (int i=0; i<(*x2-*x1+1); i++){
+    x++;
+    if (pente > 0.5){
+      p = (*y2-*y1)/(*x2-*x1);
+      if (pente < 0){
+        y++;
+      } else {
+        y--;
+      }
+    } else {
+      p = p + pente;
+    }
+    if (invert = 1) {
+      px = y;
+      py = x;
+    } else {
+      px = x;
+      py = y;
+    }
+    CLigne* row = img->getLigne(py);
+    CPixel* pix = row->getPixel(px);
+    pix->RGB(_red, _green, _blue);
+  }
 }
